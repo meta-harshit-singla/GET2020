0401537231cc1d383dd4ad1d04521ccb5f627b45
Harshit Singla <59636989+meta-harshit-singla@users.noreply.github.com>
2020-01-17 21:31:09 +0530

    Update JobScheduler.java

--- Job Scheduler/JobScheduler.java
+++ Job Scheduler/JobScheduler.java
@@ -11,117 +11,118 @@ public class JobScheduler
 	final int colsize=1;
 	
 	/**
-	 * sortarr method is used to sort array in ascending order according to their arrival time
+	 * sortArr method is used to sort array in ascending order according to their arrival time
 	 * @param rowsSize depicts the size of the row
-	 * @param localarr depicts the array which is to be worked upon
+	 * @param localArr depicts the array which is to be worked upon
 	 * @return the sorted array
 	 */
-	int[][] sortarr(int rowsSize,int localarr[][])
+	int[][] sortArr(int rowsSize,int localArr[][])
 	{
 		for(int i=0;i<rowsSize;i++)
 		{
 			for(int j=i+1;j<rowsSize;j++)
 			{
-				if(localarr[i][0]>localarr[j][0])
+				if(localArr[i][0]>localArr[j][0])
 				{
-					int temp=localarr[i][0];
-					localarr[i][0]=localarr[j][0];
-					localarr[j][0]=temp;
+					int temp=localArr[i][0];
+					localArr[i][0]=localArr[j][0];
+					localArr[j][0]=temp;
 				}
 			}
 		}
-		return localarr;
+		return localArr;
 	}
 	
 	/**
 	 * calculateCompletionTime is used to calculate completion time of the processes
-	 * @param localarr localarr depicts the sorted array which is to be worked upon
-	 * @param completionarr depicts the array with the completion time of processes 
+	 * @param localArr localarr depicts the sorted array which is to be worked upon
+	 * @param rowsSize depicts the size of the rows 
 	 * @return an array with the complete time of the processes
 	 */
 	
-	int[] calculateCompletionTime(int localarr[][],int completionarr[])
+	int[] calculateCompletionTime(int localArr[][],int rowsSize)
 	{
-	    completionarr[0]=localarr[0][0]+localarr[0][1];
-	    for(int i=1;i<localarr.length;i++)
+	    int completionArr[]=new int[rowsSize];
+	    completionArr[0]=localArr[0][0]+localArr[0][1];
+	    for(int i=1;i<localArr.length;i++)
 	    {
-	        if(completionarr[i-1]>localarr[i][0])
-	            completionarr[i]=completionarr[i-1]+localarr[i][1];
+	        if(completionArr[i-1]>localArr[i][0])
+	            completionArr[i]=completionArr[i-1]+localArr[i][1];
 	        else
-	            completionarr[i]=localarr[i][0]+localarr[i][1];
+	            completionArr[i]=localArr[i][0]+localArr[i][1];
 	    }
-	    return completionarr;
+	    return completionArr;
 	}
 	
 	/**
 	 * 
-	 * @param localarr depicts the sorted array which is to be worked upon
-	 * @param turnaroundarr depicts an array of Time taken to complete after arrival
-	 * @param completionarr depicts the array with the completion time of processes
+	 * @param localArr depicts the sorted array which is to be worked upon
+	 * @param turnAroundArr depicts an array of Time taken to complete after arrival
+	 * @param completionArr depicts the array with the completion time of processes
 	 * @return an array with the turnaround time for processes
 	 */
-	int[] calculateTurnAroundTime(int [][]localarr,int turnaroundarr[],int completionarr[])
+	int[] calculateTurnAroundTime(int [][]localArr,int turnAroundArr[],int completionArr[])
 	{
-	    for(int i=0;i<localarr.length;i++)
+	    for(int i=0;i<localArr.length;i++)
 	    {
-	        turnaroundarr[i]=completionarr[i]-localarr[i][0];
+	        turnAroundArr[i]=completionArr[i]-localArr[i][0];
 	    }
-	    return turnaroundarr;
+	    return turnAroundArr;
 	}
 	
 	/**
 	 * 
-	 * @param localarr  depicts the sorted array which is to be worked upon
-	 * @param waitingarr depicts an array of Total time the process has to wait before it's execution begins
-	 * @param turnaroundarr depicts an array of Time taken to complete after arrival
+	 * @param localArr depicts the sorted array which is to be worked upon
+	 * @param waitingArr depicts an array of Total time the process has to wait before it's execution begins
+	 * @param turnAroundArr depicts an array of Time taken to complete after arrival
 	 * @return the array with waiting time of the processes
 	 */
-	int[] calculateWaitingTime(int localarr[][],int waitingarr[],int turnaroundarr[])
+	int[] calculateWaitingTime(int localArr[][],int waitingArr[],int turnAroundArr[])
 	{
-	    for(int i=0;i<localarr.length;i++)
+	    for(int i=0;i<localArr.length;i++)
 	    {
-	        waitingarr[i]=turnaroundarr[i]-localarr[i][1];
+	        waitingArr[i]=turnAroundArr[i]-localArr[i][1];
 	    }
-	    return waitingarr;
+	    return waitingArr;
 	}
 	
 	/**
 	 * Calculates the average waiting time of the processes
-	 * @param waitingarr depicts an array of Total time the process has to wait before it's execution begins
+	 * @param waitingArr depicts an array of Total time the process has to wait before it's execution begins
 	 * @return the average waiting time of the processes
 	 */
-	double averageWaitingTime(int waitingarr[])
+	double averageWaitingTime(int waitingArr[])
 	{
-	    int totalWaitingtime=waitingarr[0];
+	    int totalWaitingtime=waitingArr[0];
 	    double averageWaitingtime=0.0;
-	    for(int i=1;i<waitingarr.length;i++)
+	    for(int i=1;i<waitingArr.length;i++)
 	    {
-	        totalWaitingtime+=waitingarr[i];
+	        totalWaitingtime+=waitingArr[i];
 	    }
-	    averageWaitingtime=(double)totalWaitingtime/(waitingarr.length);
+	    averageWaitingtime=(double)totalWaitingtime/(waitingArr.length);
 	    return averageWaitingtime ;
 	}
 	
 	/**
 	 * Calculates the maximum waiting time among the waiting time of all the processes
-	 * @param waitingarr depicts an array of Total time the process has to wait before it's execution begins
+	 * @param waitingArr depicts an array of Total time the process has to wait before it's execution begins
 	 * @return the maximum waiting time of the processes
 	 */
-	int maxWaitingTime(int waitingarr[])
+	int maxWaitingTime(int waitingArr[])
 	{
-	    int maxwaitingtime=waitingarr[0];
-	    for(int i=1;i<waitingarr.length;i++)
+	    int maximumWaitingTime=waitingArr[0];
+	    for(int i=1;i<waitingArr.length;i++)
 	    {
-	        if(maxwaitingtime<waitingarr[i])
-	            maxwaitingtime=waitingarr[i];
+	        if(maximumWaitingTime<waitingArr[i])
+	            maximumWaitingTime=waitingArr[i];
 	    }
-	    return maxwaitingtime;
+	    return maximumWaitingTime;
 	}
 	
 	public static void main(String[] args) 
 	{
-		JobScheduler obj1=new JobScheduler();
-		System.out.println("Enter the total number of processes:");
+        JobScheduler jobSchedulerInstance=new JobScheduler();
+        System.out.println("Enter the total number of processes:");
         Scanner s=new Scanner(System.in);
         int rowSize;
         boolean flag=true;
@@ -130,7 +131,6 @@ public class JobScheduler
             rowSize=s.nextInt();
             try
             {
-                
                 if(rowSize<0)
                     throw new ArithmeticException("Enter positive number");
             }
@@ -146,36 +146,35 @@ public class JobScheduler
             }
         }while(!flag);
         int burstTime,arrivalTime;
-        int[][] arr=new int[rowSize][2];
+        int[][] inputArr=new int[rowSize][2];
         for(int i=0;i<rowSize;i++)
         {
-            for(int j=0;j<obj1.colsize;j++)
+            for(int j=0;j<jobSchedulerInstance.colsize;j++)
             {
                 System.out.println("Enter the arrival time:");
                 arrivalTime=s.nextInt();
-                arr[i][j]=arrivalTime;
+                inputArr[i][j]=arrivalTime;
                 j++;
                 System.out.println("Enter the burst time:");
                 burstTime=s.nextInt();
-                arr[i][j]=burstTime;
+                inputArr[i][j]=burstTime;
             }
-        
         }
          s.close();
-		 arr=obj1.sortarr(rowSize,arr);
+		 inputArr=jobSchedulerInstance.sortArr(rowSize,inputArr);
 		 int completionTimeArr[]=new int[rowSize];
-		 completionTimeArr=obj1.calculateCompletionTime(arr, completionTimeArr);
-		 int turnaroundtimearr[]=new int[rowSize];
-		 turnAroundTimeArr=obj1.calculateTurnAroundTime(arr, turnAroundTimeArr, completionTimeArr);
+		 completionTimeArr=jobSchedulerInstance.calculateCompletionTime(inputArr,rowSize);
+		 int turnaroundTimearr[]=new int[rowSize];
+		 turnAroundTimeArr=jobSchedulerInstance.calculateTurnAroundTime(inputArr, turnAroundTimeArr, completionTimeArr);
 		 int waitingTimeArr[]=new int[rowSize];
-		 waitingTimeArr=obj1.calculateWaitingTime(arr, waitingTimeArr, TurnAroundTimearr);
+		 waitingTimeArr=jobSchedulerInstance.calculateWaitingTime(inputArr, waitingTimeArr, TurnAroundTimearr);
 		 System.out.println("Following are the processes:\n");
 		 System.out.println("Arrival Time\t  Burst Time\n");
 		 for(int i=0;i<rowSize;i++)
 		 {
-		     for(int j=0;j<=obj1.colsize;j++)
+		     for(int j=0;j<=jobSchedulerInstance.colsize;j++)
 		     {
-		         System.out.print(arr[i][j]+"\t\t  ");
+		         System.out.print(inputArr[i][j]+"\t\t  ");
 		     }
 		     System.out.println("\n");
 		 }
@@ -191,8 +190,7 @@ public class JobScheduler
          {
              System.out.println("\nTurnaround time for Process "+(i+1)+"= "+turnAroundTimeArr[i]);
          }
-		 System.out.println("\nAverage Waiting time: "+obj1.averageWaitingTime(waitingTimeArr));
-		 System.out.println("\nMaximum Waiting time: "+obj1.maxWaitingTime(waitingTimeArr));
+		 System.out.println("\nAverage Waiting time: "+jobSchedulerInstance.averageWaitingTime(waitingTimeArr));
+		 System.out.println("\nMaximum Waiting time: "+jobSchedulerInstance.maxWaitingTime(waitingTimeArr));
 	}
-
 }
