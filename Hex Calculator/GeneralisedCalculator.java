1c48f6d5c5cff2e5fd1acd30c1d460f84ed8da08
Harshit Singla <59636989+meta-harshit-singla@users.noreply.github.com>
2020-01-20 17:53:31 +0530

    Delete GeneralisedCalculator.java

--- Hex Calculator/GeneralisedCalculator.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface GeneralisedCalculator 
-{
-    public String addNumbers(String first,String second);
-    public String subtractNumbers(String first,String second);
-    public String multiplyNumbers(String first,String second);
-    public String divideNumbers(String first,String second);
-}
