f79b71f4205af9770f390a002176226db6b96abb
Harshit Singla <59636989+meta-harshit-singla@users.noreply.github.com>
2020-04-06 11:46:23 +0530

    Add files via upload

---
+++ DSA-Assignment3/DSA_Assignment3/src/priorityqueue/Queue.java
@@ -0,0 +1,26 @@
+package priorityqueue;
+
+/*
+ * interface of Queue
+ * */
+public class Queue
+{
+    int    priority;
+    String process;
+    
+    Queue(String process, int priority)
+    {
+        this.priority = priority;
+        this.process = process;
+    }
+    
+    public int getPriority()
+    {
+        return this.priority;
+    }
+    
+    public String getProcess()
+    {
+        return this.getProcess();
+    }
+}
